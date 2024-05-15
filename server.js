b2ab29763cbe535b0668586c30332f272d22b101
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2024-05-15 15:01:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const taskRoutes = require('./src/routes/taskRoutes');
 const cors = require('cors');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/tasks', taskRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
