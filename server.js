b65bbf0df4e5066bd697e41124ec8a9b3cf9c0f1
Piyush Gupta <70501930+piyushcoder1@users.noreply.github.com>
2024-05-15 15:04:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const taskRoutes = require('./src/routes/taskRoutes');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/tasks', taskRoutes);
